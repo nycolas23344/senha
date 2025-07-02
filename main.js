005efa70637a5fb39e2c1f4a0521afe2fdcb4c28
nycolas23344 <nycolas.domingues@escola.pr.gov.br>
2025-07-02 20:43:21 -0300

    ny

---
+++ main.js
@@ -0,0 +1,7 @@
+const numeroSenha-document .queryselector('.parametro-senha_texto');
+let tamanhoSenha =12;
+numeroSenha_texcontent = tamanhoSenha;
+constant letraMiusculas - 'ABCDEFJKLMNOPQRSTUVWXYZ';
+const letrasMinusculas-'abcdefghijklmnopqrstuvwxyz'; 
+cont numeros = '0123456789';
+const simbolos = '!@%*?';
